6a7e28907e05622c0e1abdb41f642f414629719f
GL3MON <harisshragavarchives@gmail.com>
2026-01-18 15:08:43 +0530

    Completed Audio Agent

--- src/SinisterSixSystems/orchestration/graph_generator.py
+++ src/SinisterSixSystems/orchestration/graph_generator.py
@@ -3,7 +3,7 @@ from SinisterSixSystems.constants import GRAPH_GENERATION_PROMPT, GRAPH_CODE_FIX
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.graph import START, END, StateGraph, MessagesState
+from langgraph.graph import START, END, StateGraph
 
 
 from typing import Dict, Any, List, Annotated, Union
